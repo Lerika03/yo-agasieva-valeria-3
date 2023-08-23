c8a1407517d8626de9d237737fbe37c557fd58cd
Lerika03 <81275988+Lerika03@users.noreply.github.com>
2023-08-23 21:14:18 +0400

    F

---
+++ 2/2.js
@@ -0,0 +1,9 @@
+
+let num = prompt("Введи число")
+let n= num, i = 1, fact = 1
+while(i<=n)
+{
+    fact = fact*i 
+    i++
+}
+console.log(fact)
---
+++ 3/3.js
@@ -0,0 +1,8 @@
+let num = prompt("Введи число")
+let degree = prompt("Введи степень") {
+        for (let num = 1; i < b; i++) {
+          result *= a;
+        }
+        console.log()
+}
+
