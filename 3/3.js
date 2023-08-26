e0401c0345612f1ef624366fa3adb546dbae1768
Lerika03 <81275988+Lerika03@users.noreply.github.com>
2023-08-26 20:18:35 +0400

    js

--- 3/3.js
+++ 3/3.js
@@ -1,9 +1,8 @@
-let num=prompt("Введите число", ""),
-
-degree=prompt("Введите степень","");
+let num=prompt("Введите число"),
+degree=prompt("Введите степень");
 let result=1;
 
 for (i=1; i<=degree; i++){
 result*=num;
 }
-console.log("Число в степени "+result);
+console.log( "Число"+ " " + num + " в степени " + degree + " равняется " + result);
--- homeworkJS/homework.js
+++ homeworkJS/homework.js
@@ -1,7 +1,11 @@
 let num = prompt("enter the number")
+if (isNaN(num)){
+    alert("this is not number")
+    num();}
+    else {
 for (let i = 0; i <= num; i++){
     if ( i %4 != 0){
     console.log(i)
 }
 }
-
+}
